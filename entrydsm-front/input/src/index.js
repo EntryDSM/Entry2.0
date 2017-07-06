49f6c3cdea28db2d45ff3c11c206f0d4d84fd464
geni429 <geni429@gmail.com>
2017-07-06 23:54:08 +0900

    앙 실수해버렸띠

--- entrydsm-front/input/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Input1, Input3, InputLayout, Footer }  from './components';
-
-const rootContent = document.getElementById('root');
-ReactDOM.render(<div><InputLayout /><Input3 /><Footer /></div>, rootContent);
