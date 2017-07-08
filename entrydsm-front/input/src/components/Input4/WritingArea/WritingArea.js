ad6d66b5576c0e9c6f7ff75e0cda01bce79fe042
XenoAron <jwt1029@naver.com>
2017-07-08 18:31:16 +0900

    Input4 WritingArea react 형식으로 패치

--- entrydsm-front/input/src/components/Input4/WritingArea/WritingArea.js
+++ entrydsm-front/input/src/components/Input4/WritingArea/WritingArea.js
@@ -2,24 +2,31 @@ import React, {Component} from 'react';
 import styles from './WritingArea.css';
 
 class WritingArea extends Component{
+    constructor(props) {
+      super(props);
+
+      this.state = {
+         count: 0
+     };
+     this.changeCount = this.changeCount.bind(this);
+   }
+    
     render() {
-        
         return(
             <div>
-                <textarea name="self_intro" className={styles.textarea_style} maxLength="1600"></textarea>
-                <p className={styles.counting_area}>(0/1600)</p>
+                <textarea name="self_intro" className={styles.textarea_style} maxLength="1600" onChange={this.changeCount}></textarea>
+                <p className={styles.counting_area}>({this.state.count}/1600)</p>
             </div>
         );
     }
 
-    componentDidMount() {
-        var siCounter = document.querySelector("textarea ~ p");
-        var siTextbox = document.getElementsByClassName("textarea_style");
-
-        siTextbox.addEventListener("input", function() {
-            siCounter.textContent =  "(" + this.value.length + "/" + 1600 + ")";
+    changeCount(e) {
+        var targetCount = e.target.value.length;
+        this.setState({
+            count: targetCount
         });
     }
+
 }
 
 export default WritingArea;
