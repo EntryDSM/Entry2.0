4fc0269403b9d9e5074dbc2d60d66018792b38ff
Terry Yun <nooheat1228@gmail.com>
2017-07-09 14:57:46 +0900

    전화번호 타입 : Number -> String

--- database/schemas/userInfo.js
+++ database/schemas/userInfo.js
@@ -7,15 +7,15 @@ let schema = mongoose.Schema({
     grade : { type : Number, default : null },
     class : { type : Number, default : null },
     protectorName : { type : String, default : null },
-    protectorTel : { type : Number, default : null },
-    phoneNum : { type : Number, default : null },
+    protectorTel : { type : String, default : null },
+    phoneNum : { type : String, default : null },
     schoolCode : { type : Number, default : null },
     schoolName : { type : String, default : null },
-    schoolTel : { type : Number, default : null },
+    schoolTel : { type : String, default : null },
     birth : { type : String, default : null },
     addressBase : { type : String, default : null },
     addressDetail : { type : String, default : null },
-    postNumber : { type : Number, default : null }
+    postNumber : { type : String, default : null }
 });
 
 module.exports = schema;
