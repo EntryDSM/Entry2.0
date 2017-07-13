fb0b5a15e992a1611093dcef4735c85d7fdcba3d
PRKM <dudgns2486@gmail.com>
2017-07-13 16:32:04 +0900

    Updated url related with applydata.

--- routes/applydata/router.js
+++ routes/applydata/router.js
@@ -1,10 +1,10 @@
 let router = require('express').Router();
 let logic = require('./logic');
 
-router.route('/step1/:userid').get(logic.loadType);
-router.route('/step1/:userid').post(logic.saveType);
-router.route('/step2/:userid').get(logic.load);
-router.route('/step2/:userid').post(logic.save);
+router.route('/classification/:userid').get(logic.loadType);
+router.route('/classification/:userid').post(logic.saveType);
+router.route('/user/info/:userid').get(logic.load);
+router.route('/user/info/:userid').post(logic.save);
 router.route('/validation').get(logic.validation);
 router.route('/preview').get(logic.demo);
 router.route('/introduce').put(logic.intro);
