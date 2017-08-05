5d69061469df355b4d4dba436ec66f08583fddeb
Jeong-minji <alswl4152@naver.com>
2017-08-05 23:20:05 +0900

    금연서약서

--- react-combine/src/components/NoSmoke.js
+++ react-combine/src/components/NoSmoke.js
@@ -42,7 +42,26 @@ class NoSmoke extends Component {
                 </div>
 
                 <div id="swear_content">
-                    Hello!!!!!!!
+                    <div id="swear_text">
+                        하나, 나 자신의 건강을 위해서 흡연을 하지 않겠습니다. <br/> 
+                        하나, 주위의 흡연의 유혹에도 절대로 흔들리지 않겠습니다. <br/>
+                        하나, 주위의 흡연을 하는 친구가 있으면 충고하여 금연을 할 수 있도록 하겠습니다. <br/>
+                        하나, 흡연은 만병의 근원임을 알고 입에도 대지 않겠습니다. <br/>
+                        하나, 상기의 조항을 위반할 경우 어떠한 처벌에도 이의를 제기하지 않겠습니다.
+                    </div>
+                    <div id="swear_me">
+                        나 <span id="blank">(</span>)은(는) 장차 소프트웨어 분야를 선도할 지도자로 성장하기 위하여 <br/>
+                        그리고 자신과 미래의 배우자 및 자녀의 건강을 배려 하여 흡연을 하지 않겠습니다. <br/>
+                        보호자 및 담임교사는 서약자가 금연을 하는데 용기와 도움을 줄 것을 약속하겠습니다.
+                    </div>
+                    <div id="swear_date">
+                        2017년 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;월 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;일
+                    </div>
+                    <div id="swear_sign">
+                        서 약 자<span id="space">:</span> ( 서&nbsp;&nbsp;명 ) <br/>
+                        보 호 자<span id="space">:</span> ( 서&nbsp;&nbsp;명 ) <br/>
+                        담임교사<span id="space">:</span>( 서&nbsp;&nbsp;명 ) <br/>
+                    </div>
                 </div>
             </div>
         )
