e0263caf7d38a060b427b96b51816ec94f6e8de7
Jeong-minji <alswl4152@naver.com>
2017-08-30 10:35:02 +0900

    self intro

--- react-combine/src/components/StudyPlan.js
+++ react-combine/src/components/StudyPlan.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 class StudyPlan extends Component {
      render() {
         return(
-            <div id="study_plan">
+            <div id="selfintroduce">
                 <div id="introduce_header">
-                    <span id="header">학업계획서</span>
+                    <span id="header">자기소개서</span>
                 </div>
                 <div id="userinfo">
                     <table id="userinfo_table">
@@ -26,7 +26,7 @@ class StudyPlan extends Component {
                         <tbody>
                             <tr id="explain">
                                 <td id="content_header">
-                                    <span id="content_title">◎ 학업계획서</span>는 자신이 본교를 선택하게 된 구체적인 사유(지원 동기)와 고등학생이 된 후 이루고자 하는 목표를 달성하기 위해 생각하는 학업계획을 상세하게 기술하십시오.
+                                    <span id="content_title">◎ 자기소개서</span> 내용은 특별한 형식은 없으며 개인의 특성 및 성장 과정, 취미‧특기, 학교생활, 가족 안에서의 역할, 남들보다 뛰어나다고 생각하는 자신의 장점(특성 혹은 능력)과 보완 ‧ 발전시켜야 할 단점에 대하여 기술하십시오.
                                 </td>
                             </tr>
                             <tr id="content">
