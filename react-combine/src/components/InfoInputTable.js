177e0f59a996a9e85ae635f0552df6d9909d2b64
geni429 <geni429@gmail.com>
2017-09-04 18:34:26 +0900

    InfoInput 수정

--- react-combine/src/components/InfoInputTable.js
+++ react-combine/src/components/InfoInputTable.js
@@ -11,7 +11,7 @@ class InfoInputTable extends React.Component {
                     <tbody>
                         <tr id="tr_name">
                             <td className="td_title">성명</td>
-                            <td className="td_content"><input type="text" className="input_style" readOnly/></td>
+                            <td className="td_content"><input type="text" className="input_style" value={this.props.name} readOnly/></td>
                             <td className="td_space" rowSpan="6"><UploadImage /></td>
                         </tr>
                         <tr>
@@ -33,7 +33,7 @@ class InfoInputTable extends React.Component {
                             <td className="td_title">학반</td>
                             <td className="td_content">
                                 &nbsp;&nbsp;&nbsp;3학년
-                                <input type="text" className="input_style" id="class"/>반 
+                                <input type="text" className="input_style" id="class" onChange={this.props.setClass}/>반 
                             </td>
                         </tr>
                         <tr>
@@ -45,38 +45,38 @@ class InfoInputTable extends React.Component {
                         <tr>
                             <td className="td_title">본인연락처</td>
                             <td className="td_content">
-                                <input type="text" className="input_style" id="my_tel"/>
+                                <input type="text" className="input_style" id="my_tel" onChange={this.props.setPhoneNum}/>
                             </td>
                         </tr>
                         <tr>
                             <td className="td_title">보호자명</td>
                             <td className="td_content">
-                                <input type="text" className="input_style" id="parent_name"/>
+                                <input type="text" className="input_style" id="parent_name" onChange={this.props.setParentsName}/>
                             </td>
                         </tr>
                         <tr>
                             <td className="td_title">부모연락처</td>
                             <td className="td_content" colSpan="2">
-                                <input type="text" className="input_style" id="parent_tel"/>
+                                <input type="text" className="input_style" id="parent_tel" onChange={this.props.setParentsTel}/>
                             </td>
                         </tr>
                         <tr>
                             <td className="td_title">학교연락처</td>
                             <td className="td_content" colSpan="2">
-                                <input type="text" className="input_style" id="school_tel"/>
+                                <input type="text" className="input_style" id="school_tel" onChange={this.props.setSchoolTel}/>
                             </td>
                         </tr>
                         <tr>
                             <td className="td_title">이메일</td>
                             <td className="td_content" colSpan="2">
-                                <input type="text" className="input_style" readOnly/>@<input type="text" className="input_style" readOnly/>
+                                <input type="text" className="input_style" value={this.props.email} readOnly/>@<input type="text" className="input_style" value={this.props.emailDomain} readOnly/>
                             </td>
                         </tr>
                         <tr id="tr_address">
                             <td className="td_title">주소</td>
                             <td className="td_content" colSpan="2">
                                 <input type="text" className="input_style" id="base_address" readOnly/><AddressModal />
-                                <input type="text" className="input_style" id="detail_address"/>
+                                <input type="text" className="input_style" id="detail_address" onChange={this.props.setDetailAddress}/>
                             </td>
                         </tr>
                     </tbody>
