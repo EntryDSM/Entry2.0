325165511967ef27503ea3119790fd5431573fef
geni429 <geni429@gmail.com>
2017-09-07 19:46:19 +0900

    attend 초기값 설정 및 로직 변경

--- react-combine/src/containers/GradeInput.js
+++ react-combine/src/containers/GradeInput.js
@@ -16,19 +16,61 @@ class GradeInput extends Component{
             postData: {
 
             },
+            absence: 0,
+            late: 0,
+            leaving_early: 0,
+            not_attendence: 0,
             graduated: null,
             graduate_to_be: "hide",
             black: null
         };
     }
 
+    setAttendData(e){
+        switch(e.target.name){
+            case "absence": {
+                this.setState({
+                    absence: e.target.value
+                })
+                break;
+            }
+            case "late": {
+                this.setState({
+                    late: e.target.value
+                })
+                break;
+            }
+            case "leaving_early": {
+                this.setState({
+                    leaving_early: e.target.value
+                })
+                break;
+            }
+            case "not_attendence": {
+                this.setState({
+                    not_attendence: e.target.value
+                })
+                break;
+            }
+        }
+    }
+
     render(){
+        let attendData = [
+            this.state.absence,
+            this.state.late,
+            this.state.leaving_early,
+            this.state.not_attendence
+        ];
+        console.log(attendData);
         return(
             <div id="contents">
                 <InputHeader now={"성적입력"}/>
                 <div id="volunteerAttendWrapper">
                     <Volunteer />
-                    <Attend />
+                    <Attend 
+                        setAttendData={this.setAttendData.bind(this)}
+                        attendValue={attendData} />
                 </div>
                 <table id="grade_input_table">
                     <Graduate visible={this.state.graduate_to_be}/>
