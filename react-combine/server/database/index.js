55c07dad481688cfd6ffd5519e2c9c8d3fffa921
Terry Yun <nooheat1228@gmail.com>
2017-09-14 14:14:37 +0900

    [USE] node-cmd to auto backup

--- react-combine/server/database/index.js
+++ react-combine/server/database/index.js
@@ -1,18 +1,20 @@
 var mongoose = require('mongoose');
-const backup = require('mongodb-backup');
+const cmd = require('node-cmd');
 
 module.exports = () => {
+    const db_host = process.env.ENTRYDSM_DB_URL.split(':')[1].slice(2);
+    const db_port = process.env.ENTRYDSM_DB_URL.split(':')[2].split('\/')[0];
+    const db_name = process.env.ENTRYDSM_DB_URL.split('\/')[3];
+
     mongoose.Promise = global.Promise;
     mongoose.connect(process.env.ENTRYDSM_DB_URL);
     mongoose.connection.on('error', console.error.bind(console, 'mongoose connection error.'));
     mongoose.connection.on('open', function () {
         console.log('CONNECTED TO DATABASE');
         setInterval(() => {
-            backup({
-                uri: 'mongodb://localhost:27017/EntryDSM',
-                root: __dirname + '/../../DB-EntryDSM'
-            });
-        }, 60 * 60 * 1000)
+            console.log('DATABASE BACKED UP');
+            cmd.run(`mongodump --out ${__dirname + '/../../../../DB-EntryDSM'} --host ${db_host} --port ${db_port} --db ${db_name}`)
+        }, 5 * 1000)
     });
     mongoose.connection.on('disconnected', () => {
         console.log('DISCONNECTED FROM DATABASE')
