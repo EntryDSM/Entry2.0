2636a32d861741b2fc9ede5dbcaf201bd66ca9a6
KoreanJungYJ <jung22337@naver.com>
2017-09-19 14:06:34 +0900

    Errorpage is completed

--- react-combine/src/containers/ErrorPage.js
+++ react-combine/src/containers/ErrorPage.js
@@ -6,6 +6,10 @@ class ErrorPage extends Component{
         return(
             <div id = "error-page">
                 <ErrorHeader ImgUrl = {require('../images/DSM Logo.png')}/>
+                <ErrorMain ImgUrl1 = {require('../images/warning.png')}
+                           ImgUrl2 = {require('../images/monitor.png')}
+                           title1 = "Error!"
+                           title2 = " 잘못된 접근입니다! "/>
             </div>
         );
     }
@@ -15,6 +19,16 @@ const ErrorHeader = (props) => {
     return(
         <div id = "errorHeader">
             <img src = {props.ImgUrl} alt = "logo" id = "logo"/>
+        </div>
+    );
+}
+
+const ErrorMain = (props) => {
+    return(
+        <div>
+            <img src = {props.ImgUrl1} alt = "img" id = "warningImg"/>
+            <h1> {props.title1} </h1>
+            <h2> "{props.title2}" </h2>
         </div>
     );
 }
