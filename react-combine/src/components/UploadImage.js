052f8f8dd4fffec7f2419a5ee53db84358c892ca
geni429 <geni429@gmail.com>
2017-09-20 08:19:03 +0900

    uploadimage debuging

--- react-combine/src/components/UploadImage.js
+++ react-combine/src/components/UploadImage.js
@@ -2,19 +2,9 @@ import React, {Component} from 'react';
 
 class UploadImage extends Component {
     render() {
-        let src;
-        axios({
-            method: 'get',
-            url: '/api/upload/profile'
-        }).then(response => {
-            src = require('/api/upload/profile');
-        }).catch(err => {
-            console.log(err);
-            src = require('../images/file.png');
-        })
         return(
             <div className="div_style">
-                <img src={src} id="IDPhoto"/>
+                <img src={require('../images/file.png')} id="IDPhoto"/>
                 <input type="file" id="attachfile" onChange={this.props.previewFile} accept="image/gif,image/jpeg,image/jpg,image/png"/> 
                 {/*Icon made by [Interaction Assets] from www.flaticon.com -->*/}
             </div>
