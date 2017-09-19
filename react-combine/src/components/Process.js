817e45506c07f4d8cc464210cc40185c0d7ef2a6
geni429 <geni429@gmail.com>
2017-09-19 13:10:44 +0900

    processBar routing

--- react-combine/src/components/Process.js
+++ react-combine/src/components/Process.js
@@ -8,10 +8,10 @@ class Process extends Component{
                 <svg id="svg">
                     <path d="M60 20 L60 500" stroke="white" strokeWidth="11"/>
                     <circle className="point" cx="60" cy="20" r="16" stroke-width="3" />
-                    <circle className="point" cx="60" cy="120" r="16" stroke-width="3" />
-                    <circle className="point" cx="60" cy="220" r="16" stroke-width="3" />
-                    <circle className="point" cx="60" cy="320" r="16" stroke-width="3" />
-                    <circle className="point" cx="60" cy="420" r="16" stroke-width="3" />
+                    <circle className="point" cx="60" cy="120" r="16" stroke-width="3" name="classification" onClick={this.props.moveClassification} />
+                    <circle className="point" cx="60" cy="220" r="16" stroke-width="3" name="infoinput" onClick={this.props.moveInfoinput} />
+                    <circle className="point" cx="60" cy="320" r="16" stroke-width="3" name="gradeinput" onClick={this.props.moveGradeinput} />
+                    <circle className="point" cx="60" cy="420" r="16" stroke-width="3" name="introduce" onClick={this.props.moveIntroduce} />
                     <text x="100" y="28" fill="white">기본정보</text>
                     <text x="100" y="128" fill="white">구분선택</text>
                     <text x="100" y="228" fill="white">인적사항</text>
