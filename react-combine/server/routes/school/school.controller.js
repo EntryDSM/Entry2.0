a25b8cc56db547a0864ce14c58387e210abdeec5
Terry Yun <nooheat1228@gmail.com>
2017-10-11 16:14:25 +0900

    [FIX] School Searching BUG

--- react-combine/server/routes/school/school.controller.js
+++ react-combine/server/routes/school/school.controller.js
@@ -18,7 +18,7 @@ function search(name, goverment) {
     return new Promise((resolve, reject) => {
         let search = {};
         if (typeof name !== "undefined") search.name = new RegExp(name.split('').join('.{0,}'));
-        if (typeof goverment !== "undefined") search.goverment = goverment;
+        if (typeof goverment !== "undefined") search.simpleGoverment = goverment;
 
         resolve(SchoolCode.find(search, {
             "_id": false,
