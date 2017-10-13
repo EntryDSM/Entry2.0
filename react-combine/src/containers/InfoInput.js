596fe417daa52760f86f19974e70b7b79e78329c
geni429 <geni429@gmail.com>
2017-10-13 14:27:17 +0900

    연락처 문제 해결

--- react-combine/src/containers/InfoInput.js
+++ react-combine/src/containers/InfoInput.js
@@ -71,18 +71,18 @@ class InfoInput extends Component {
                     }
                 })
                 console.log(birth);
-                if(!response.data.tel === ""){
+                if(!response.data.tel == ""){
                     phoneNum = response.data.tel.split('-');
                 } else {
                     phoneNum = ["", "", ""];
                 }
-                if(!response.data.parentsTel === ""){
+                if(!response.data.parentsTel == ""){
                     parentsTel = response.data.parentsTel.split('-');                
                 } else {
                     console.log(response.data.parentsTel.split('-').length);
                     parentsTel = ["", "", ""];
                 }
-                if(!response.data.schoolTel === ""){
+                if(!response.data.schoolTel == ""){
                     schoolTel = response.data.schoolTel.split('-');
                 } else {
                     schoolTel = ["", "", ""];
