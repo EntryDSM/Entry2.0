c7fc9f0d42691883f66625b8accffcdadd40c6ff
Terry Yun <nooheat1228@gmail.com>
2017-10-18 15:44:36 +0900

    [FIX] typo

--- react-combine/server/database/models/SchoolCode.js
+++ react-combine/server/database/models/SchoolCode.js
@@ -2,14 +2,14 @@ let mongoose = require('mongoose');
 
 let School = mongoose.Schema({
     code : {type : Number , default : null },
-    goverment : {type : String ,default :""},
+    government : {type : String ,default :""},
     name : {type : String,default :""},
-    fullGoverment : {type:String , default: ""}
+    simpleGovernment : {type:String , default: ""}
 },{collection : 'SchoolCode'});
 
 
 School.methods.isRegionMismatch = function (regionType) {
-    if (regionType === 'HOME') return !this.goverment.includes('대전');
-    if (regionType === 'AWAY') return this.goverment.includes('대전');
+    if (regionType === 'HOME') return !this.government.includes('대전');
+    if (regionType === 'AWAY') return this.government.includes('대전');
 }
 module.exports = mongoose.model('SchoolCode', School);
