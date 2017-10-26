ff4422a2780f7b8fa80e2411a3b12786e0f81254
KoreanJungYJ <jung22337@naver.com>
2017-10-26 14:33:32 +0900

    final commit

--- react-combine/src/index.js
+++ react-combine/src/index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import 'babel-polyfill';
-import { Classification, GradeInput, InfoInput, Introduce, Landing, SignIn, MainPage, Preview, SendInfo, SignUp, ChangePw, PwSendComplete, FinalSubmit, SignupSendComplete, MyPage, ErrorPage, Validation, Board, FinalSubmitErrorPage, InternalErrorPage, FinishPage } from './containers';
+import { FinishPage } from './containers';
 import reducer from './reducers';
 import InputLayout from './components/InputLayout';
 
@@ -16,29 +16,7 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(
     <Router history={browserHistory}>
         <Route path="/">
-            <IndexRoute component={Landing} />
-            <Route path="/main" component={MainPage} />
-            <Route path="/signin" component={SignIn} />
-            <Route path="/changepw" component={ChangePw} />
-            <Route path="/sendinfo" component={SendInfo} />
-            <Route path="/pwsendcomplete" component={PwSendComplete} />
-            <Route path="/SignupSendComplete" component={SignupSendComplete} />
-            <Route path="/mypage" component={MyPage}/>
-            <Route path="/error" component={ErrorPage}/>
-            <Route path="/finalError" component={FinalSubmitErrorPage}/>
-            <Route path="/internalError" component={InternalErrorPage}/>
-            <Route path="/validation" component={Validation} />
-            <Route path="/board" component={Board}/>
-            <Route path="/input" component={InputLayout}>
-                <Route path="/signup" component={SignUp} />
-                <Route path="/classification" component={Classification} />
-                <Route path="/infoinput" component={InfoInput} />
-                <Route path="/gradeinput" component={GradeInput} />
-                <Route path="/introduce" component={Introduce} />
-                <Route path="/preview" component={Preview} />
-                <Route path="/finalsubmit" component={FinalSubmit} />
-            </Route>
-            <Route path = "/finish" component={FinishPage}/>
+            <IndexRoute component={FinishPage} />
         </Route>
     </Router>
 , rootElement);
